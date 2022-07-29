0e693fcdd8c1608a8cada6730b98587a5afd1f80
Oğuzhan Türkmen <oguzhn.1132@gmail.com>
2022-07-29 20:13:08 +0300

    first commit

---
+++ app/src/main/java/com/oguzhanturkmen/newsapp/SelectListener.java
@@ -0,0 +1,7 @@
+package com.oguzhanturkmen.newsapp;
+
+import com.oguzhanturkmen.newsapp.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void onNewsClicked(NewsHeadlines headlines);
+}
